215d09d1ca8f71f5845c11c0bebc2ccd494a712a
sallyibrahim117 <sallyibrahim117@gmail.com>
2024-09-06 02:38:26 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// linkItem=document.querySelector(".list-item");
+// function show(params) {
+    
+// }
+let num=document.querySelector(".timer");
+let num2=document.querySelector(".timer-2");
+
+increase(num,400)
+increase(num2,80,40)
+function increase(el,number,timer=10){
+    let counter=0;
+let interval=setInterval(() => {
+    counter+=10;
+    el.innerHTML=counter;
+    if(counter>=number){
+        clearInterval(interval)
+    }
+}, timer);
+}
+
+
+var products;
+var xhr=new XMLHttpRequest();
+xhr.open('get','http://kdkdoko');
+xhr.onreadystatechange=function(){
+    if(xhr.readyState==4&&xhr.status==200){
+        console.log(xhr.responseText);
+        products=xhr.responseText;
+    }
+}
+xhr.send();
